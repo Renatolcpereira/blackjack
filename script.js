e8a32f1de5784ef4cec7c293a906eeee9ea1f142
Renatolcpereira <105220981+Renatolcpereira@users.noreply.github.com>
2022-07-12 12:02:36 -0300

    Add files via upload
    Changes of 12 julho 2022

--- script.js
+++ script.js
@@ -2,11 +2,14 @@
     'use strict';
 
     // Array cartas do baralho
-    let allCards = ["p1","p2","p3","p4","p5","p6","p7","p8","p9","p10","p11","p12","p13",
+    /* let allCards = ["p1","p2","p3","p4","p5","p6","p7","p8","p9","p10","p11","p12","p13",
             "e1","e2","e3","e4","e5","e6","e7","e8","e9","e10","e11","e12","e13",
             "c1","c2","c3","c4","c5","c6","c7","c8","c9","c10","c11","c12","c13",
-            "o1","o2","o3","o4","o5","o6","o7","o8","o9","o10","o11","o12","o13"]
+            "o1","o2","o3","o4","o5","o6","o7","o8","o9","o10","o11","o12","o13"] */
+            let allCards = ["p1","p2","p3","p4","p5","p6","p7","p8","p9","p10","p11","p12","p13",
+            "e1","e2","e3","e4"]
     const totalcards = (allCards.length);
+    let totalUsedCards = 0;
 
     // dealer vars
     let dealerTotalCards = 0;
@@ -70,22 +73,60 @@
         document.getElementById('control').style.display = "block";
 
         getBoardsPositions();
-
-        // Add initials cards
-        if ( true ){
-            dealerTotalCards++;
-            dealerPack.innerHTML += `<div id="cpcard1${pl1TotalCards+1}" style="left: ${CardsPosX-dealerCardPosX}px; top: ${CardsPosY-dealerCardPosY}px;"><img src="img/back-blue.jpg"></div>`;
-            
-            dealerTotalCards++;
-            dealerPack.innerHTML += `<div id="cpcard1${pl1TotalCards+1}" style="left: ${CardsPosX-dealerCardPosX}px; top: ${CardsPosY-dealerCardPosY}px;"><img src="img/back-blue.jpg"></div>`;
-            
-            pl1TotalCards++;
-            playerPack.innerHTML += `<div id="p1card1${dealerTotalCards+1}" style="left: ${CardsPosX-pl1CardPosX}px; top: ${CardsPosY-pl1CardPosY}px;"><img src="img/back-blue.jpg"></div>`;
-            
-            pl1TotalCards++;
-            playerPack.innerHTML += `<div id="p1card1${dealerTotalCards+1}" style="left: ${CardsPosX-pl1CardPosX}px; top: ${CardsPosY-pl1CardPosY}px;"><img src="img/back-blue.jpg"></div>`;
-        }
-       
+        setTimeout( dealerCardMoveFunc, 0);
+        setTimeout( dealerCardMoveFunc, 600);
+        setTimeout( PlayerCardMoveFunc, 1200);
+        setTimeout( PlayerCardMoveFunc, 1800);
+              
     })
+
+    // Callback functions to call cardMove()
+    function dealerCardMoveFunc() {  
+        dealerTotalCards++;
+        dealerPack.innerHTML += `<div id="dealercard1${dealerTotalCards}" style="left: ${CardsPosX-dealerCardPosX}px; top: ${CardsPosY-dealerCardPosY}px;"><img src="img/back-blue.jpg"></div>`;
+        cardMove(`dealercard1${dealerTotalCards}`, CardsPosX-dealerCardPosX, CardsPosY-dealerCardPosY, (dealerTotalCards-1)*15, 0 );
+        setTimeout(showCardFunc ,600, `dealercard1${dealerTotalCards}`)
+    }
+
+    function PlayerCardMoveFunc() {  
+        pl1TotalCards++;
+        playerPack.innerHTML += `<div id="p1card1${pl1TotalCards}" style="left: ${CardsPosX-pl1CardPosX}px; top: ${CardsPosY-pl1CardPosY}px;"><img src="img/back-blue.jpg"></div>`;
+        cardMove(`p1card1${pl1TotalCards}`, CardsPosX-pl1CardPosX, CardsPosY-pl1CardPosY, (pl1TotalCards-1)*15, 0 );
+        setTimeout(showCardFunc ,600, `p1card1${pl1TotalCards}`)
+    }
+
+    // Function to move cards
+    function cardMove(card, posX1, posY1, posX2, posY2) {
+        let id = null;
+        let i = 0;
+        let elem = document.getElementById(card);
+        let constX = (posX1 -posX2) / 100;
+        let constY = (posY2 -posY1) / 100;
+        console.log(card);
+        console.log(`posX1: ${posX1}, posY1: ${posY1}, posX2: ${posX2}, posY2: ${posY2}`);
+        console.log(`constX: ${constX} e constY: ${constY}`);
+
+        clearInterval(id);
+        id = setInterval(frame, 5);
+
+        function frame() {
+            if (i > 100) {
+                clearInterval(id);
+            } else {
+                i++
+                console.log(i);
+                posY1 += constY
+                posX1 -= constX
+                elem.style.top = Math.round(posY1) + "px"; 
+                elem.style.left = Math.round(posX1) + "px"; 
+            }
+        }
+               
+    }
+
+    function showCardFunc (myCard) {  
+        totalUsedCards++;
+        document.getElementById(myCard).innerHTML = `<img src="img/${allCards[totalUsedCards-1]}.jpg">`; 
+    }
 
 })();
